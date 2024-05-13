2ae5f568adb1db0a8e7c738c490854ae2b2a25f5
Prajwal Y R <prajwalyr6612@gmail.com>
2024-05-13 21:07:12 +0530

    vertexpace project

---
+++ script.js
@@ -0,0 +1,38 @@
+const loginPopup = document.querySelector(".login-popup");
+  const close = document.querySelector(".close");
+
+
+  window.addEventListener("load",function(){
+ 
+   showPopup();
+   // setTimeout(function(){
+   //   loginPopup.classList.add("show");
+   // },5000)
+
+  })
+
+  function showPopup(){
+        const timeLimit = 8// seconds;
+        let i=0;
+        const timer = setInterval(function(){
+         i++;
+         if(i == timeLimit){
+          clearInterval(timer);
+          loginPopup.classList.add("show");
+         } 
+         console.log(i)
+        },1000);
+  }
+
+
+  close.addEventListener("click",function(){
+    loginPopup.classList.remove("show");
+  })
+
+
+  //flip box//
+  document.querySelectorAll('.flip-box').forEach((flipBox) => {
+    flipBox.addEventListener('click', () => {
+        flipBox.querySelector('.flip-box-inner').style.transform = 'rotateY(180deg)';
+    });
+});
